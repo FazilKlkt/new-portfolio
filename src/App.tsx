ed5a3ce0802904d4541e002153ac4d58415f87d1
Fazil Husen <fazil.husen6@gmail.com>
2024-07-21 21:40:41 +0530

    basic layout design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [text1, setText1] = useState("Fazil");
+  const [text2, setText2] = useState("Husen");
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello!</h1>
+      <div className="flex justify-around  m-4 rounded-md outline outline-1 min-h-[7vh]">
+        <div className="self-center text-xl">{text1}</div>
+        <div className="m-3 h-10 w-10 rounded-full outline outline-1"></div>
+        <div className="self-center text-xl">{text2}</div>
+      </div>
+      <div className="flex justify-around  mx-40 rounded-md outline outline-1 min-h-[75vh]">
+        {/* <div className="self-center text-xl">{text1}</div>
+        <div className="m-3 h-10 w-10 rounded-full outline outline-1"></div>
+        <div className="self-center text-xl">{text2}</div> */}
+      </div>
+      <div className="flex justify-around  m-4 rounded-md outline outline-1 min-h-[7vh]">
+        {/* <div className="self-center text-xl">{text1}</div> */}
+        {/* <div className="m-3 h-10 w-10 rounded-full outline outline-1"></div> */}
+        {/* <div className="self-center text-xl">{text2}</div> */}
+      </div>
     </>
   );
 }
